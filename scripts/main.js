37ef93b7e8b9148ba2e26474738f9961587dbf93
Benjamin <benja.ignacio.0515@gmail.com>
2025-02-12 13:47:45 -0300

    add a bit of js

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+function multiplica(num1, num2) {
+  let resultado = num1 * num2;
+  return resultado;
+}
+
+let miImage = document.querySelector("img");
+miImage.onclick = function () {
+  let miSrc = miImage.getAttribute("src");
+  if (miSrc === "images/cat.jpg") {
+    miImage.setAttribute("src", "images/cat2.jpg");
+  } else {
+    miImage.setAttribute("src", "images/cat.jpg");
+  }
+};
+
+let miBoton = document.querySelector("button");
+let miTitulo = document.querySelector("h1");
+
+function estableceNombreUsuario() {
+  let miNombre = prompt("Por favor, ingresa tu nombre");
+  if (!miNombre) {
+    estableceNombreUsuario();
+  } else {
+    localStorage.setItem("nombre", miNombre);
+    miTitulo.innerHTML = "Bienvenido, " + miNombre;
+  }
+}
+
+if (!localStorage.getItem("nombre")) {
+  estableceNombreUsuario();
+} else {
+  let nombreAlmacenado = localStorage.getItem("nombre");
+  miTitulo.textContent = "Bienvenido, " + nombreAlmacenado;
+}
+
+miBoton.onclick = function () {
+  estableceNombreUsuario();
+};
